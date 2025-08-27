95b60b0368e37d41c5457d8c147692de83aab0db
WELLINGTON CARDOSO CAMILO <wellington.ccamilo@senacsp.edu.br>
2025-08-26 21:21:42 -0300

    modificando 2 arquivos python

--- kfomfrom.py
+++ kfomfrom.py
@@ -3,3 +3,18 @@ def soma(x, y):
 
 result = soma(10, 10)
 print(result)
+
+def subtrair(x, y):
+    return x - y
+
+result = subtrair(10, 10)
+print(result)
+
+def mult(x, y):
+    return x * y
+
+result = mult(10, 10)
+print(result)
+
+
+print(90 + 90)
---
+++ mfkmklrdm.py
@@ -0,0 +1,4 @@
+print("wellinton")
+print("wellinton")
+print("wellinton")
+print("wellinton")
